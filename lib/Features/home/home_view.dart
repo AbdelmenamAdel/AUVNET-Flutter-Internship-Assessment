c423d091905657960299604f5b19bd04d8a98365
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2025-06-17 22:47:05 +0300

    home is done

--- lib/Features/home/home_view.dart
+++ lib/Features/home/home_view.dart
@@ -1,18 +1,33 @@
+import 'package:auvnet_internship_task/Features/home/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
+import 'widgets/resturant_sectioin.dart';
+import 'widgets/services_section.dart';
+import 'widgets/shourtcut_section.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      extendBodyBehindAppBar: true,
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Text(
-          'Home View',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // ! AppBar
+          CustomAppBar(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10.0),
+            child: Column(
+              spacing: 16,
+              children: [
+                ServicesSection(),
+                ShortCutSection(),
+                ResturantSection(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
